43bc66e0b5e22c2af2d38080bc9b73a6e16af642
diwasupreti508-collab <diwasupreti508@gmail.com>
2026-02-05 11:35:03 +0545

    initial submit

--- src/q10.c
+++ src/q10.c
@@ -1,1 +1,18 @@
 // Write a C program to swap the values of two variables using a temporary variable.
+
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("value of a after swap is %d and b is %d\n", a, b);
+
+    return 0;
+}
--- src/q15.c
+++ src/q15.c
@@ -1,1 +1,15 @@
 // Write an expression that checks if a number is both positive and even.
+
+#include <stdio.h>
+int main()
+{
+    int n=10;
+    if (n>0 && (n & 1) == 0){
+        printf("%d is both positive and even\n", n);
+
+    }
+    
+    return 0;
+
+
+}
