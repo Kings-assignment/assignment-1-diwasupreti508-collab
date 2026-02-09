8442ae3f50beee5a24dfac24b983afba3c9b1a2d
diwasupreti508-collab <diwasupreti508@gmail.com>
2026-02-09 10:23:13 +0545

    final submit

--- src/q8.c
+++ src/q8.c
@@ -1,3 +1,4 @@
 /* Write a C program to convert temperature from Celsius to Fahrenheit. Prompt the user for a
 temperature in Celsius and display the equivalent temperature in Fahrenheit.
 (Formula: fahrenheit = (celsius * 9 / 5) + 32) */
+
